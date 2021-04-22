1202fdcba2a6bf8f79ad3aed837d6ce18f76de89
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-22 15:56:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,14 +4,14 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    if
+    ifint % 3 && 5 == 0
     
     "Fizz"
     
     
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 && 5 == 0
+  elsif 
     "FizzBuzz"
   elsif int % 3 || 5 == 0
     nil
